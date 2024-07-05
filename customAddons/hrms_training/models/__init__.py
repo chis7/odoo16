bf0a56a258587b7ac87be5fd14bb50994f9eeed5
chis7 <chisenga.musonda@yahoo.com>
2024-07-05 07:57:16 +0200

    training evaluation integrated successfully

--- customAddons/hrms_training/models/__init__.py
+++ customAddons/hrms_training/models/__init__.py
@@ -3,3 +3,6 @@ from . import typeOfTraining
 from . import fieldOfStudy
 from . import levelOfStudy
 from . import training
+from . import end_of_course_evaluation
+from . import post_training_evaluation
+from . import supervisors_post_training_evaluation
