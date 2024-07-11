c336501eff87b72d45a98155994c595e557505ff
chis7 <chisenga.musonda@yahoo.com>
2024-07-11 14:48:34 +0200

    added email function for notifying employees about training plan

--- customAddons/hrms_training/models/__init__.py
+++ customAddons/hrms_training/models/__init__.py
@@ -6,3 +6,5 @@ from . import training
 from . import end_of_course_evaluation
 from . import post_training_evaluation
 from . import supervisors_post_training_evaluation
+from . import provinces
+from . import sponsor
--- customAddons/hrms_training/models/trainingPlan_BACKUPB4REDESIGN.py
+++ customAddons/hrms_training/models/trainingPlan_BACKUPB4REDESIGN.py
@@ -8,10 +8,8 @@ class TrainingPlan(models.Model):
     _name = "training.plan"
     _inherit = ["mail.thread"]
     _description = "training plan"
-    # name = fields.Char(string="Ministry", required=True)
-    name = fields.Many2one("res.company", string="Ministry")
-    province = fields.Many2one("training.province", string="Province")
-    # province = fields.Char(string="Province", required=True)
+    name = fields.Char(string="Ministry", required=True)
+    province = fields.Char(string="Province", required=True)
     institution = fields.Char(string="Institution", required=True)
     yearOfPlan = fields.Char(string="Year", required=True)
     supervisor_id = fields.Many2one("hr.employee", string="Supervisor")
@@ -38,8 +36,10 @@ class TrainingPlan(models.Model):
     psComment = fields.Text(string="PS Comment", tracking=True)
     psmdComment = fields.Text(string="PSMD Comment", tracking=True)
 
+
     def action_save_training_plan_as_draft(self):
         self.write({'state': 'draft'})
+
 
     def action_submit_training_plan_to_hrdc(self):
         self.write({'state': 'hrdc'})
@@ -58,22 +58,10 @@ class TrainingPlan(models.Model):
 
     def action_psmd_approve_training_plan(self):
         self.write({'state': 'approved'})
-        # Assuming 'mail.template' ID for the email to be sent
-
-        for line in self.training_plan_line_ids:
-            employee = line.employee_id
-            if employee.user_id and employee.user_id.email:
-                # Sending email
-                mail_values = {
-                    'subject': 'Training Approval',
-                    'body_html': '<p>You have been shortlisted to attend a training and your trainined has been approved</p>',
-                    'email_to': employee.user_id.email,
-                }
-                mail = self.env['mail.mail'].create(mail_values)
-                mail.send()
 
     def action_psmd_send_back_training_plan(self):
         self.write({'state': 'ps'})
+
 
     @api.model
     def create(self, vals):
@@ -83,6 +71,12 @@ class TrainingPlan(models.Model):
         res = super(TrainingPlan, self).create(vals)
         return res
 
+    # @api.model
+    # def create(self, vals):
+    #     if vals.get('reference', _('Invalid')) == _('Invalid'):
+    #         vals['reference'] = self.env['ir.sequence'].next_by_code('training.plan') or _('Invalid')
+    #     res = super(TrainingPlan, self).create(vals)
+    #     return res
 
 class TrainingPlanLines(models.Model):
     _name = "training.plan.lines"
@@ -90,13 +84,29 @@ class TrainingPlanLines(models.Model):
     employee_id = fields.Many2one('hr.employee', string="Candidate Name", required=True, store=True)
     priorityRanking = fields.Integer(string="Priority Ranking", required=True, store=True)
     priorityArea = fields.Char(string="Priority Area", required=True, store=True)
-
+    department = fields.Char(string="Department", required=True, store=True)
+    nrcNumber = fields.Char(string="NRC Number", required=True, store=True)
+    pmecNumber = fields.Char(string="PMEC Number", required=True, store=True)
+    gender = fields.Selection(
+        selection=[
+            ("male", "Male"),
+            ("female", "Female"),
+        ],
+        default='female',
+        string="Gender",
+        required=True,
+        store=True
+    )
+    position = fields.Char(string="Position", required=True, store=True)
+    job_description = fields.Text(string="Job Description", required=True, store=True)
+    age = fields.Integer(string="Age (years)", required=True, store=True)
+    candidate_level_of_training = fields.Char(string="Candidate's Level of Training", required=True, store=True)
     name = fields.Char(string="Training Proposed", required=True, store=True)
     level_of_training_proposed = fields.Char(string="Level of Training Proposed", required=True, store=True)
     location_of_programme_proposed = fields.Many2one("res.country", "Location of Programme Proposed", required=True,
                                                      store=True)
     proposed_date_of_programme = fields.Date(string="Proposed Date of Programme", required=True, store=True)
-    sponsor = fields.Many2one('training.sponsor', string="Sponsor", required=True, store=True)
+    sponsor = fields.Many2one('hr.employee', string="Sponsor", required=True, store=True)
     justification_of_training = fields.Text(string="Justification of Training", required=True, store=True)
     estimated_cost_per_year = fields.Integer(string="Estimated Cost per Year", required=True, store=True)
     training_plan_id = fields.Many2one('training.plan', string="Training Plan", required=True, store=True)
